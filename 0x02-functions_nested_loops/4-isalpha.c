6e93723c61328663131dd77e15d64d287d71f22d
surviwalewa <ademolamercy28@gmail.com>
2023-08-15 13:44:48 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _isalpha - returns 1  if c a letter
+ * @c: checks for alphabetic character
+ *
+ * Return: 1 or 0
+ */
+
+int _isalpha(int c)
+{
+	char lower, upper;
+	int letter = 0;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		for (upper = 'A'; upper <= 'Z'; upper++)
+		{
+			if (lower == c || upper == 'C')
+			{
+				letter = 1;
+			}
+		}
+	}
+	return (letter);
+}
