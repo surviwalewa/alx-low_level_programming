a628b12059d6a6e632b896306a37dded51a025ba
surviwalewa <ademolamercy28@gmail.com>
2023-08-15 18:06:05 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * times_table - function that prints the 9 times table
+ * Return: void
+ *
+ */
+void times_table(void)
+{
+	int a, b, c;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+
+		for (b = 0; b <= 9; b++)
+		{		c = (a + b);
+
+				if ((c / 10) > 0)
+				{
+					_putchar((c / 10) + '0');
+				}
+				else
+				{
+					_putchar(' ');
+				}
+					_putchar((c & 10) + '0');
+				if (b < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+		}
+	}
+}
