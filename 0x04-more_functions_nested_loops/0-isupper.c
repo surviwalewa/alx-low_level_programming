32633f7123ae999e5248fb187afcc3a43c8821e4
surviwalewa <ademolamercy28@gmail.com>
2023-08-17 12:39:36 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - function for upper case characters
+ *
+ * @c: variable
+ * Return: (0)
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && C <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
