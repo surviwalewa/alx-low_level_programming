b975143c8a4589299f6a8396446d2700bd323d44
surviwalewa <ademolamercy28@gmail.com>
2023-08-17 21:15:03 -0700

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - program that finds and prints the largest prime factor of the number
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	long prime = 612852475143, divisor;
+
+	while (divisor < (prime / 2))
+	{
+		if ((prime % 2) == 0)
+		{
+			prime /= 2;
+			continue;
+		}
+		for (divisor = 3; divisor < (prime / 2); divisor += 2)
+		{
+			if ((prime % divisor) == 0)
+				prime /= divisor;
+		}
+	}
+	printf("%ld\n", prime);
+
+	return (0);
+}
+
