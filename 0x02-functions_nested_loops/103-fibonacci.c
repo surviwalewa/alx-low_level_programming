849eb5d783b1a95375160a5482b0711ea18f09cf
surviwalewa <ademolamercy28@gmail.com>
2023-08-18 00:30:06 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - sum of the even-valued terms
+ * Return: 0
+ */
+int main(void)
+{
+	long int i, j = 1, k = 2, sum = 0, tsum = 0;
+
+	for (i = 0; i < 49; i++)
+	{
+		if ((k % 2 == 0) && (k <= 4000000))
+		{
+			tsum = tsum + k;
+		}
+		sum = j + k;
+		j = k;
+		k = sum;
+	}
+	return (0);
+}
