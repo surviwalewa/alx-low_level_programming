5c3192fba614c5bc8bac03583f9cad356c7c29c7
surviwalewa <ademolamercy28@gmail.com>
2023-10-05 23:01:43 -0700

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -41,6 +41,29 @@ void check_elf(unsigned char *e_ident)
 }
 
 /**
+ * print_magic - Prints the magic numbers of an ELF header.
+ * @e_ident: A pointer to an arr which contains d ELF magic nos
+ *
+ * Description: Magic numbers are separated by spaces.
+ */
+void print_magic(unsigned char *e_ident)
+{
+	int index;
+
+	printf(" Magic: ");
+
+	for (index = 0; index < EI_NIDENT; index++)
+	{
+		printf("%02x", e_ident[index]);
+
+		if (index == EI_NIDENT - 1)
+			printf("\n");
+		else
+			printf(" ");
+	}
+}
+
+/**
  * print_class - func that prints d class of an ELF header.
  * @e_ident: A pointer to an arr that contains d ELF class.
  */
